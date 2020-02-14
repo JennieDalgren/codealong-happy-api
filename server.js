9cf76b0353058ce52d7fd323bc5ecbcb7051e47b
Jennie Dalgren <jennie.dalgren@gmail.com>
2020-02-14 14:58:25 +0100

    setting up backend

--- server.js
+++ server.js
@@ -1,24 +1,85 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/happyThoughts'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to mongodb 🎉')
+})
+
+mongoose.connection.on('error', err => {
+  console.error('connection error:', err)
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Setup model
+const Thought = mongoose.model('Thought', {
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 120
+  },
+  hearts: {
+    type: Number,
+    require: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  tag: {
+    type: String
+  }
 })
 
-// Start the server
+// Setup routes
+app.get('/', async (req, res) => {
+  const thoughts = await Thought.find()
+    .sort({ createdAt: 'desc' })
+    .limit(20)
+  res.json(thoughts)
+})
+
+app.post('/', async (req, res) => {
+  const thought = new Thought({
+    message: req.body.message,
+    hearts: 0
+  })
+  try {
+    const saved = await thought.save()
+    res.status(201).json(saved)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'could not save thought', errors: err.errors })
+  }
+})
+
+app.post('/:id/like', async (req, res) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.id },
+      { $inc: { hearts: 1 } },
+      { new: true }
+    )
+    res.json(thought)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'coudl not update heart', errors: err.errors })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
